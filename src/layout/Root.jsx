a762a080b4eae34078996e3bbb39067505ac2794
alifhasanshah <alifhasanshah@gmail.com>
2023-11-07 19:24:14 +0600

    basic routing done

---
+++ src/layout/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Root = () => {
+  return (
+    <div>
+        <header></header>
+        <main>
+            <Outlet />
+        </main>
+        <footer></footer>
+    </div>
+  )
+}
+
+export default Root
