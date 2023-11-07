5076a45819e316bfb7349f043fe9b36f917a4ed6
alifhasanshah <alifhasanshah@gmail.com>
2023-11-08 01:25:35 +0600

    footer done\

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const RootLayout = () => {
   return (
     <div>
-        <header className='bg-base-300'>
-          <Navbar />
-        </header>
-        <main>
-            <Outlet />
-        </main>
-        <footer></footer>
+      <header className="bg-base-300">
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer className="bg-[#252525]  w-full ">
+        <Footer />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
