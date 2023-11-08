d90f5ab72013bae2b98abb8d82303d1c667e2e3a
alifhasanshah <alifhasanshah@gmail.com>
2023-11-09 03:33:43 +0600

    show all assignments in card

--- src/pages/AllAssignments/AllAssignments.jsx
+++ src/pages/AllAssignments/AllAssignments.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import useAxios from "../../hooks/useAxios";
+import SingleAssignment from "./SingleAssignment";
 
 const AllAssignments = () => {
-  return (
-    <div>AllAssignments</div>
-  )
-}
+  const [assignments, setAssignments] = useState([])
+  const axios = useAxios();
 
-export default AllAssignments
+
+  useEffect( () => {
+    axios.get('/all-assignments')
+    .then(res => {
+      setAssignments(res.data);
+    })
+  }, [])
+
+  return (
+    <>
+      <div>
+        <div className="container mx-auto px-6 py-24">
+          <div className="flex justify-between border-2 border-green-500 py-9 px-6 rounded-lg ">
+            <div>
+              <h2 className="text-3xl font-normal">
+                Filter Assignments based on Difficulty
+              </h2>
+            </div>
+            <div>
+              <label
+                htmlFor="assignmentType"
+                className="font-semibold text-lg mr-4"
+              >
+                Select Difficulty Level{" "}
+              </label>
+              <select
+                className="w-40 py-2 bg-gray-200 rounded-md focus:outline-none"
+                name="assignmentType"
+                id="assignmentType"
+                required
+              >
+                <option value="">Select Type</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+          </div>
+          {/* show all assignments */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-14 pb-9">
+            {
+              assignments?.map( (assignment) => (
+                <SingleAssignment key={assignment?._id} assignment={assignment} />
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AllAssignments;
---
+++ src/pages/AllAssignments/SingleAssignment.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SingleAssignment = ({ assignment }) => {
+  const {
+    _id,
+    title,
+    thumbnail,
+    marks,
+    description,
+    dueDate,
+    difficultyLevel,
+  } = assignment;
+  console.log(assignment);
+
+  return (
+    <>
+      <div>
+        <div className="card  bg-base-100 shadow-xl">
+          <div className="relative">
+            <figure className="py-6 ">
+              <img
+                className="h-[350px] w-[450px] object-cover  "
+                src="https://i.postimg.cc/wBGSRGmD/courses-1.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="px-8 pb-8">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-md font-bold ">{title}</h2>
+            </div>
+
+            <div className="mt-4">
+              <p className="text-sm text-gray-400">
+                {description.length > 100
+                  ? description.slice(0, 100) + "..."
+                  : description}
+              </p>
+            </div>
+            <div className="mt-4">
+              <p className="text-lg text-gray-500 font-bold">
+                Total Marks : {marks}
+              </p>
+            </div>
+            <div className="flex justify-around mt-4">
+              <div>
+                <Link>
+                  {" "}
+                  <button className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    See Details
+                  </button>
+                </Link>
+              </div>
+              <div>
+                <Link to={"/update-assignment"}>
+                  <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    Update Info{" "}
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleAssignment;
